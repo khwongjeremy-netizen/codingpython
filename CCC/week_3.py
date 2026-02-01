3478e1579379c8c2cead3b48175cd70bf3a57c91
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-01 18:59:29 +0000

    iugkhjhg

--- CCC/week_3.py
+++ CCC/week_3.py
@@ -1,16 +1,19 @@
+play = ['A', 'B', 'C', 'D', 'E']
 while True:
     b = int(input('B: '))
     n = int(input('N: '))
-    play = ['A', 'B', 'C', 'D', 'E']
     if b == 1:
         for i in range(n):
-            play = [play[:3]] + [play[0]]
+            play = play[1:] + [play[0]]
+            print(play)
     elif b == 2:
         for i in range(n):
-            play = [play[4]] + [play[:3]]
+            play = [play[-1]] + play[:-1]
+            print(play)
     elif b == 3:
         for i in range(n):
             play[1], play[0] = play[0], play[1]
+            print(play)
     elif b == 4: 
         print(play)
         break
