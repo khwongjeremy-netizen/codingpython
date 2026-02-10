3248b688083feb6c4294fe9d63ff4dffd066220c
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-10 00:12:28 +0000

    dsfsdf

--- CCC/week _1.py
+++ CCC/week _1.py
@@ -9,9 +9,15 @@ output:
 25 22 12 0 5
 30 27 17 5 0
 '''
-cities = []
+cities = [0,]
+output = []
 for i in range(4):
     city = int(input('distance: '))
     cities.append(city)
 
-    
+for i in range(len(cities)):
+    output = []
+    for j in range(len(cities)):
+        dist = abs(cities[i] - cities[j])
+        output.append(dist)
+    print(output)
