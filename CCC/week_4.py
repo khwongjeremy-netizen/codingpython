59090ebce56d0ad82dd1a3031ab3404e458cff80
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-13 23:56:16 +0000

    khjghjk

--- CCC/week_4.py
+++ CCC/week_4.py
@@ -13,26 +13,4 @@ while True:
         print("No such integer exists")
         break
 
-    '''
-input:
-3 10 12 5
-
-output:
-0 3 13 25 30
-3 0 10 22 27
-13 10 0 12 17
-25 22 12 0 5
-30 27 17 5 0
-'''
-cities = [0,]
-output = []
-for i in range(4):
-    city = int(input('distance: '))
-    cities.append(city)
-
-for i in range(len(cities)):
-    output = []
-    for j in range(len(cities)):
-        dist = abs(cities[i] - cities[j])
-        output.append(dist)
-    print(output)
+    
