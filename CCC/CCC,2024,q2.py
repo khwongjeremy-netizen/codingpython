7c32c0ec753b734e5978a11a887971eca05c2b29
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-18 23:26:35 +0000

    sfasdfs

--- CCC/CCC,2024,q2.py
+++ CCC/CCC,2024,q2.py
@@ -1,6 +1,14 @@
 Dusa = int(input())
 yobi = []
+size = 0
 while True:
     yobi_size = int(input())
-    if yobi_size == ''
+    if yobi_size == '':
+        break
     yobi.append(int(yobi_size))
+
+for i in range(len(yobi)):
+    if yobi[i] > size:
+        size += yobi[i]
+
+print(size)
