dd4eda35725af252a706666c7f5e70b69c4d5342
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-21 17:56:26 +0000

    woijfiwje

--- CCC/connecting territories.py
+++ CCC/connecting territories.py
@@ -1,17 +1,27 @@
-length = input()
-width = input()
-division = int(input())
-grid = int(length)*int(width) 
-layout = []
-index = 0
-for i in range(grid):
-    index += 1
-    if index <= division:
-        layout.append(index)
-    else: 
-        index = 1
+row = int(input())
+column = int(input())
+mark = int(input())
 
-print(layout)
-for i in range(layout):
-
-    
+a = [0] * row
+m = 0
+for r in range(row):
+    for col in range(column):
+        m += 1
+        if m > mark:
+            m = 1
+            if r == 0:
+                a[col] = m
+            else:
+                if col == 0: 
+                    min_current = min(a[0], a[1])
+                    min_left = min_current
+                elif col < column - 1:
+                    min_right = min(a[col], a[col+1])
+                    min_current = min(min_left, min_right)
+                    min_left = min_right
+                else:
+                    min_current = min_left
+                
+                a[col] = min_current + m
+result = min(a)
+print(result)
