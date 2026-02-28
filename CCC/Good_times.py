1af75af4fa8e83157075ac3f23d7a54d3e658ba8
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-02-28 22:26:52 +0000

    lefvn

---
+++ CCC/Good_times.py
@@ -0,0 +1,27 @@
+input_time = int(input())
+
+zones = ['Ottawa', 'Victoria', 'Edmonton', 'Winnipeg', 'Toronto', 'Halifax', "St. John's"]
+time = 0
+st_johns = 0
+times = []
+for i in range(len(zones)):
+    if zones[i] == zones[0]:
+        time += input_time
+    if zones[i] == zones[1]:
+        time += input_time- 300
+    if zones[i] == zones[2]:
+        time += input_time- 200
+    if zones[i] == zones[3]:
+        time += input_time- 100
+    if zones[i] == zones[4]:
+        time += input_time
+    if zones[i] == zones[5]:
+        time += input_time + 100
+    if zones[i] == zones[6]:
+        time += input_time + 130
+    if time < 2400:
+        times.append(time)
+    else:
+        time -= 2400 
+        times.append(time)
+
