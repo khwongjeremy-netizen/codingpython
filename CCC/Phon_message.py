decbdc62fec547d981daf3682657c4c3130c697d
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-03-02 23:51:37 +0000

    ojio

--- CCC/Phon_message.py
+++ CCC/Phon_message.py
@@ -15,17 +15,15 @@ halt
 13
 7
 '''
-#ones 
-one = ['a', 'd', 'g', 'j', 'm', 'p', 't', 'w']
 
-#twos
-two = ['b','e', 'h','k','n', 'q', 'u', 'x']
-
-#threes
-three = ['c', 'f', 'i', 'l', 'o', 'r', 'v', 'y']
-
-#fours 
-four = ['s', 'z']
+one = ['a', 'b', 'c']
+two = ['d', 'e', 'f']
+three = ['g', 'h', 'i']
+four = ['j', 'k','l']
+five = ['m', 'n', 'o']
+six = ['p', 'q', 'r', 's']
+seven = ['t', 'u', 'v']
+eight = ['w', 'x' , 'y', 'z']
 
 pause = 2
 messages = []
@@ -40,19 +38,38 @@ while True:
 for mess in range(len(messages)):
     time = 0
     current = messages[mess]
+    condition = ''
     for i in range(len(current)):
         if i+1 != len(current):
             if current[i] == current[i+1]:
                 time += 2
-            if 
+            
+            if condition == condition:
+                time += 2
         if current[i]in one:
             time += 1
+            condition = 'one'
         if current[i] in two:
-            time += 2
+            time += two
+            condition = 'two'
         if current[i] in three: 
             time += 3
+            condition = 'three'
         if current[i] in four: 
             time += 4
+            condition = 'four'
+        if current[i] in five: 
+            time += 4
+            condition = 'five'
+        if current[i] in six: 
+            time += 4
+            condition = 'six'
+        if current[i] in seven: 
+            time += 4
+            condition = 'seven'
+        if current[i] in eight: 
+            time += 4
+            condition = 'eight'
         
     timetotal.append(time)
 
