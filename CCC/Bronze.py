5e73d1bee370ae60ca49beef3c8ae092d770039b
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-03-12 06:25:45 +0000

    awdviqenrc

--- CCC/Bronze.py
+++ CCC/Bronze.py
@@ -1,21 +1,21 @@
-'''
-8
-75
-70
-60
-70
-70
-60
-75
-70
 
-60 2
->>>>>>>>
-4
-70
-62
-58
-73
+num = int(input())
+contest = []
+for i in range(num):
+    score = int(input())
+    contest.append(score)
+omits = []
+for j in range(3):
+    best = 0
+    for i in range(len(contest)):
+        if contest[i] > best and contest[i] not in omits:
+            best = contest[i]
+    omits.append(best)
 
-62 1
-'''
+print(omits)
+bronze = omits[2]
+amt = 0
+for i in contest:
+    if i == bronze:
+        amt += 1
+print(str(bronze) + " " + str(amt))
