f2a541bdefc5380554a782ba9a1fca6327a5aff4
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2026-04-21 03:25:33 +0000

    python

--- CCC/mockccc24J3.py
+++ CCC/mockccc24J3.py
@@ -5,14 +5,17 @@ out:
 2
 '''
 integer = int(input())
-word = input().lower()
-
-
+word = input().upper()
+start = False
+mid = False
 total = 0
-for i in range(word):
-    first = i
-    second = i+1
-    third = i+3
-    if word[first] == 'r' and word[second] == 'g' and word[third] == 'b':
-        total += 1
+for i in range(integer):
+    if word[i] == 'R':
+        start = True
+        for j in range(integer-i):
+            if word[j] == 'G':
+                mid = True
+            if word[j] == 'B':
+                if start and mid:
+                    total += 1
 print(total)
